23f88619c683a272bf5843b1def481b88040dc11
He-Martijn <roert.stapje_0o@icloud.com>
2022-04-10 15:34:08 +0200

    Fix named parameters

--- lib/story.dart
+++ lib/story.dart
@@ -4,7 +4,7 @@ class Story{
   String choice2;
 
 
-  Story(this.storyTitle,this.choice1,this.choice2){
+  Story({this.storyTitle,this.choice1,this.choice2}){
     print('An instance of the class Story had been created.');
   }
 
